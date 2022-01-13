9835d01c15a3a5b74941b5358c8e8d6c8d3a3af8
k0s02oi <krishnamurthy.sannidhi@walmart.com>
2022-01-13 05:47:48 -0600

    added pdf view package and rendered in app js

--- App.js
+++ App.js
@@ -10,20 +10,29 @@
 import React from 'react';
 import {
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   View,
   Image,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import Slider from '@react-native-community/slider';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import SoundPlayer from 'react-native-sound';
+import Pdf from 'react-native-pdf';
 
 import {listSpeedValues} from './src/constants';
 import {useAudioHelper} from './src/helpers/audio-helper';
+
+import samplPdf from './src/assets/thereactnativebook-sample.pdf';
+const pdfSource = {
+  uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+  cache: true,
+};
 
 const App = () => {
   const player = useAudioHelper({
@@ -54,87 +63,97 @@ const App = () => {
     timeRate: 15,
     isLogStatus: true,
   });
+
   return (
-    <View style={styles.container}>
-      <Text> RN Audio POC</Text>
-      <View style={{backgroundColor: 'black'}}>
-        <Text style={styles.name}>Name: {player.currentAudioName}</Text>
-        <View style={styles.changeAudio}>
-          <TouchableOpacity onPress={player.previous}>
-            <FontAwesomeIcon
-              name="step-backward"
-              size={50}
-              color={
-                player.isDisabledButtonPrevious === false ? 'white' : 'gray'
-              }
-            />
-          </TouchableOpacity>
-          <Image
-            source={require('./src/images/nezuko.png')}
-            style={styles.avatar}
-          />
-          <TouchableOpacity onPress={player.next} style={styles.button}>
-            <FontAwesomeIcon
-              name="step-forward"
-              size={50}
-              color={player.isDisabledButtonNext === false ? 'white' : 'gray'}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.actionButtonsOther}>
-          <TouchableOpacity onPress={player.decreaseTime} style={styles.button}>
-            <FontAwesomeIcon name="rotate-left" size={50} color="white" />
-            <Text
-              style={{
-                position: 'absolute',
-                alignSelf: 'center',
-                marginTop: 1,
-                color: 'white',
-                fontSize: 12,
-              }}>
-              {player.timeRate}
-            </Text>
-          </TouchableOpacity>
-          {player.status === 'play' ? (
-            <TouchableOpacity
-              onPress={player.pause}
-              style={{marginHorizontal: 20}}>
-              <FontAwesomeIcon name="pause" color="white" size={50} />
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity
-              onPress={player.play}
-              style={{marginHorizontal: 20}}>
-              <FontAwesomeIcon name="play" color="white" size={50} />
-            </TouchableOpacity>
-          )}
-          <TouchableOpacity onPress={player.increaseTime} style={styles.button}>
-            <FontAwesomeIcon name="rotate-right" size={50} color="white" />
-            <Text
-              style={{
-                position: 'absolute',
-                alignSelf: 'center',
-                marginTop: 1,
-                color: 'white',
-                fontSize: 12,
-              }}>
-              {player.timeRate}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.actionButtonsOther}>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={{textAlign: 'center'}}> RN Audio POC</Text>
+          <View
+            style={{backgroundColor: '#E37963', margin: 25, borderRadius: 25}}>
+            <Text style={styles.name}>Album: {player.currentAudioName}</Text>
+            <View style={styles.changeAudio}>
+              <TouchableOpacity onPress={player.previous}>
+                <FontAwesomeIcon
+                  name="step-backward"
+                  size={30}
+                  color={
+                    player.isDisabledButtonPrevious === false ? 'white' : 'gray'
+                  }
+                />
+              </TouchableOpacity>
+              <Image
+                source={require('./src/images/audio_static.jpeg')}
+                style={styles.avatar}
+              />
+              <TouchableOpacity onPress={player.next} style={styles.button}>
+                <FontAwesomeIcon
+                  name="step-forward"
+                  size={30}
+                  color={
+                    player.isDisabledButtonNext === false ? 'white' : 'gray'
+                  }
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.actionButtonsOther}>
+              <TouchableOpacity
+                onPress={player.decreaseTime}
+                style={styles.button}>
+                <FontAwesomeIcon name="rotate-left" size={30} color="white" />
+                <Text
+                  style={{
+                    position: 'absolute',
+                    alignSelf: 'center',
+                    marginTop: 1,
+                    color: 'white',
+                    fontSize: 12,
+                  }}>
+                  {player.timeRate}
+                </Text>
+              </TouchableOpacity>
+              {player.status === 'play' ? (
+                <TouchableOpacity
+                  onPress={player.pause}
+                  style={{marginHorizontal: 20}}>
+                  <FontAwesomeIcon name="pause" color="white" size={30} />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  onPress={player.play}
+                  style={{marginHorizontal: 20}}>
+                  <FontAwesomeIcon name="play" color="white" size={30} />
+                </TouchableOpacity>
+              )}
+              <TouchableOpacity
+                onPress={player.increaseTime}
+                style={styles.button}>
+                <FontAwesomeIcon name="rotate-right" size={30} color="white" />
+                <Text
+                  style={{
+                    position: 'absolute',
+                    alignSelf: 'center',
+                    marginTop: 1,
+                    color: 'white',
+                    fontSize: 12,
+                  }}>
+                  {player.timeRate}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            {/* <View style={styles.actionButtonsOther}>
           <TouchableOpacity onPress={player.shuffle} style={styles.button}>
             <EntypoIcon
               name="shuffle"
               color={player.isShuffle === true ? '#3399ff' : 'white'}
-              size={50}
+              size={30}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={player.loop} style={styles.button}>
             <MaterialIcon
               name="loop"
               color={player.isLoop === true ? '#3399ff' : 'white'}
-              size={50}
+              size={30}
             />
           </TouchableOpacity>
           <TouchableOpacity
@@ -144,7 +163,7 @@ const App = () => {
             <EntypoIcon
               name="controller-stop"
               color={player.status === 'stop' ? 'red' : 'white'}
-              size={50}
+              size={30}
             />
           </TouchableOpacity>
           {player.isMuted === false ? (
@@ -166,54 +185,86 @@ const App = () => {
             thumbTintColor="#FFFFFF"
             onSlidingComplete={volume => player.setVolume(volume)}
           />
-        </View>
-        <View style={styles.progressBar}>
-          <Text style={styles.progressBarText}>{player.currentTimeString}</Text>
-          <Slider
-            style={{width: '70%', height: 40}}
-            minimumValue={0}
-            maximumValue={player.duration}
-            value={player.currentTime}
-            minimumTrackTintColor="#FFFFFF"
-            maximumTrackTintColor="gray"
-            thumbTintColor="#FFFFFF"
-            onTouchStart={player.pause}
-            onTouchEnd={player.play}
-            onSlidingComplete={seconds => player.seekToTime(seconds)}
-          />
-          <Text style={styles.progressBarText}>{player.durationString}</Text>
-        </View>
-        <View style={styles.speed}>
-          {listSpeedValues.map((item, index) => (
-            <TouchableOpacity
-              key={index}
-              style={styles.speedItem}
-              onPress={() => player.setSpeed(item.value)}>
-              <Text
-                style={{
-                  color: player.speed === item.value ? '#3399ff' : 'white',
-                }}>
-                {item.text}
+        </View> */}
+            <View style={styles.progressBar}>
+              <Slider
+                style={{width: '100%', height: 40}}
+                minimumValue={0}
+                maximumValue={player.duration}
+                value={player.currentTime}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="gray"
+                thumbTintColor="#FFFFFF"
+                onTouchStart={player.pause}
+                onTouchEnd={player.play}
+                onSlidingComplete={seconds => player.seekToTime(seconds)}
+              />
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={styles.progressBarText}>
+                {player.currentTimeString}
               </Text>
-            </TouchableOpacity>
-          ))}
+              <Text style={styles.progressBarText}>
+                {player.durationString}
+              </Text>
+            </View>
+            <View style={styles.speed}>
+              {listSpeedValues.map((item, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={styles.speedItem}
+                  onPress={() => player.setSpeed(item.value)}>
+                  <Text
+                    style={{
+                      color: player.speed === item.value ? '#3399ff' : 'white',
+                    }}>
+                    {item.text}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+          <View style={{color: 'black'}}>
+            <Pdf
+              source={samplPdf}
+              onLoadComplete={(numberOfPages, filePath) => {
+                console.log(`Number of pages: ${numberOfPages}`);
+              }}
+              onPageChanged={(page, numberOfPages) => {
+                console.log(`Current page: ${page}`);
+              }}
+              onError={error => {
+                console.log(error);
+              }}
+              onPressLink={uri => {
+                console.log(`Link pressed: ${uri}`);
+              }}
+              style={styles.pdfStyle}
+            />
+          </View>
         </View>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'black',
+    //backgroundColor: 'lightblue',
   },
   name: {
     color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    margin: 10,
   },
   avatar: {
-    width: 200,
-    height: 200,
     alignSelf: 'center',
     borderRadius: 100,
     margin: 15,
@@ -225,7 +276,6 @@ const styles = StyleSheet.create({
   },
   progressBarText: {
     color: 'white',
-    alignSelf: 'center',
   },
   speed: {
     width: '100%',
@@ -261,6 +311,12 @@ const styles = StyleSheet.create({
   },
   button: {
     justifyContent: 'center',
+  },
+  pdfStyle: {
+    flex: 1,
+    margin: 10,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
 
